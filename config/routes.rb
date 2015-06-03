df1c7ef30c2d35bf692d83029843a7418f6ca823
Jennifer Mah <jen.mah@hotmail.com>
2015-06-03 09:49:20 +0100

    added to the routes.rb file so that localhost:3000/ now directs users to the index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'festivals#index'
   resources :festivals
 
   # You can have the root of your site routed with "root"
